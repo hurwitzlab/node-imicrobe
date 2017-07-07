4e3b09df36b42655766881cc9c3c24a1e45ff118
Ken Youens-Clark <kyclark@gmail.com>
2017-07-07 10:07:03 -0700

    commit while waiting for node to compile

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 const express = require('express')
 const app = express()
+const port = 3006;
+
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'kyclark',
+    password : 'L1ttlecrobes.',
+    database : 'imicrobe'
+});
 
 app.get('/', function (req, res) {
-  var i = 5;
-  res.send({ msg: 'Hello World!', num : i })
+  connection.connect();
+
+  connection.query('SELECT * from investigators', 
+    function (error, results, fields) {
+      if (error) throw error;
+      res.send(results);
+    }
+  );
+
+  connection.end();
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(port, function () {
+  console.log('Example app listening on port ' + port)
 })
