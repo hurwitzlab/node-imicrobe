561b692af0e9feba603a4810038975a8054060b7
Ken Youens-Clark <kyclark@gmail.com>
2017-07-21 16:27:27 -0700

    imma workin on the railroad

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
-"use strict";
+'use strict';
 
-const express = require('express')
-const app = express()
-const port = 3006;
-var cors = require('cors')
-var Promise = require('promise');
-var printf = require('printf');
-var config = require('./config.json');
+const express   = require('express');
+const app       = express();
+const port      = 3006;
+const MONGO_URL = 'mongodb://localhost:27017/imicrobe';
+var cors        = require('cors');
+var Promise     = require('promise');
+var printf      = require('printf');
+var config      = require('./config.json');
+var MongoClient = require('mongodb').MongoClient;
+var assert      = require('assert');
+var bodyParser  = require('body-parser');
+var jsonParser  = bodyParser.json();
 
-app.use(cors())
+app.use(cors());
+
 
 
 // --------------------------------------------------
@@ -20,19 +26,11 @@ var connection = mysql.createConnection(config.mysql);
 app.get('/investigators/:id(\\d+)', function (req, res) {
   var id = req.params.id;
   console.log("/investigators/" + id);
-  connection.query(
-    'select * from investigator where investigator_id=?', 
-    [id], 
-    function (error, results, fields) {
-      if (error) throw error;
-      if (results.length == 1) {
-        res.json(results[0]);
-      }
-      else {
-        res.status(404).send("Bad investigator id: " + id);
-      }
-    }
-  );
+  getInvestigator(id)
+    .then(getProjectsForInvestigator)
+    .then(getSamplesForInvestigator)
+    .then((data) => res.json(data))
+    .catch((err) => res.status(500).send(err));
 })
 
 
@@ -40,18 +38,10 @@ app.get('/investigators/:id(\\d+)', function (req, res) {
 app.get('/investigators', function (req, res) {
   console.log("/investigators");
 
-  connection.query(
-    `select   investigator_id, investigator_name, 
-              institution
-     from     investigator 
-     order by investigator_name
-    `,
-    function (error, results, fields) {
-      if (error) throw error;
-      res.json(results);
-    }
-  );
-})
+  getInvestigators()
+    .then((data) => res.json(data))
+    .catch((err) => res.status(500).send(err));
+});
 
 
 // --------------------------------------------------
@@ -62,8 +52,10 @@ app.get('/projects/:id(\\d+)', function (req, res) {
     .then(getDomainsForProject)
     .then(getInvestigatorsForProject)
     .then(getPublicationsForProject)
-    .then(function (data) { res.json(data) });
-})
+    .then(getSamplesForProject)
+    .then((data) => res.json(data))
+    .catch((err) => res.status(500).send(err) );
+});
 
 
 // --------------------------------------------------
@@ -72,16 +64,8 @@ app.get('/projects', function (req, res) {
   getProjects()
     .then(getDomainsForProjects)
     .then(getInvestigatorsForProjects)
-    .then( function (data) { res.json(data) }
-  );
-});
-
-
-// --------------------------------------------------
-app.get('/search/:query', function (req, res) {
-  var query = req.params.query;
-  console.log("/search/" + query);
-  getSearchResults(query).then( (data) => res.json(data) );
+    .then((data) => res.json(data))
+    .catch((err) => res.status(500).send(err));
 });
 
 
@@ -89,14 +73,50 @@ app.get('/search/:query', function (req, res) {
 app.get('/samples/:id(\\d+)', function (req, res) {
   var id = req.params.id;
   console.log("/samples/" + id);
-  getSample(id).then( (data) => res.json(data) ) ;
+  getSample(id)
+    .then(getInvestigatorsForProject)
+    .then(getFilesForSample)
+    .then(getOntologiesForSample)
+    .then((data) => res.json(data))
+    .catch((err) => res.status(500).send(err));
 });
 
 
 // --------------------------------------------------
 app.get('/samples', function (req, res) {
   console.log("/samples");
-  getSamples().then( (data) => res.json(data) ) ;
+  getSamples()
+    .then((data) => res.json(data))
+    .catch((err) => res.status(500).send(err));
+});
+
+
+// --------------------------------------------------
+app.post('/samplesearch', jsonParser, function (req, res) {
+  console.log("/samplesearch");
+  connectMongo()
+    .then((db)   => getMetaSearchResults(db, req.body))
+    .then((data) => res.json(data))
+    .catch((err) => res.status(500).send(err));
+});
+
+// --------------------------------------------------
+app.get('/search_params', function (req, res) {
+  console.log("/search_params");
+  connectMongo()
+    .then((db)   => getSampleKeys(db))
+    .then((data) => res.json(data))
+    .catch((err) => res.status(500).send(err));
+});
+
+
+// --------------------------------------------------
+app.get('/search/:query', function (req, res) {
+  var query = req.params.query;
+  console.log("/search/" + query);
+  getSearchResults(query)
+    .then((data) => res.json(data))
+    .catch((err) => res.status(500).send(err));
 });
 
 
@@ -114,7 +134,7 @@ app.get('/', function(req, res){
 // catch-all function
 // 
 app.get('*', function(req, res){
-  res.status(404).send("Unknown route: " + req.path);
+  res.status(500).send("Unknown route: " + req.path);
 });
 
 
@@ -136,6 +156,24 @@ function getProject(id) {
           resolve(results[0]);
         else 
           reject("Bad project id: " + id);
+      }
+    );
+  });
+}
+
+
+// --------------------------------------------------
+function getInvestigators() {
+  return new Promise(function (resolve, reject) {
+    connection.query(
+      `select   investigator_id, investigator_name, 
+                institution
+       from     investigator 
+       order by investigator_name
+      `,
+      function (error, results, fields) {
+        if (error) reject(error);
+        resolve(results);
       }
     );
   });
@@ -278,35 +316,6 @@ function getSearchResults(query) {
 
 
 // --------------------------------------------------
-/* FIXME desired query is below, file count was removed due to error
-select  s.sample_id, s.sample_name, s.sample_type,
-                   p.project_id, p.project_name,
-                   s.latitude, s.longitude,
-                   d.domain_name,
-                   count(f.sample_file_id) as num_files
-        from       sample s
-        inner join project p
-        on         s.project_id=p.project_id
-        left join  sample_file f
-        on         s.sample_id=f.sample_id
-        left join  project_to_domain p2d
-        on         p.project_id=p2d.project_id
-        left join  domain d
-        on         p2d.domain_id=d.domain_id
-
-      ` select     s.sample_id, s.sample_name, s.sample_type,
-                   p.project_id, p.project_name,
-                   s.latitude, s.longitude,
-                   d.domain_name
-        from       sample s
-        inner join project p
-        on         s.project_id=p.project_id
-        left join  project_to_domain p2d
-        on         p.project_id=p2d.project_id
-        left join  domain d
-        on         p2d.domain_id=d.domain_id
-      `,
-*/
 function getSamples() {
   return new Promise(function (resolve, reject) {
     connection.query(
@@ -318,9 +327,211 @@ function getSamples() {
         where  s.project_id=p.project_id
       `,
       function (error, results, fields) {
-        if (error) throw error;
+        if (error) reject(error);
         resolve(results);
       }
     );
+  });
+}
+
+
+// --------------------------------------------------
+function getSamplesForProject(project) {
+  return new Promise(function (resolve, reject) {
+    connection.query(
+      `
+        select s.sample_id, s.sample_acc, s.sample_name,
+               s.sample_type, s.sample_description, s.comments,
+               s.taxon_id, s.url, s.latitude, s.longitude
+        from   sample s
+        where  s.project_id=?
+
+      `,
+      [project.project_id],
+      function (error, results, fields) {
+        if (error) reject(error);
+        project['samples'] = results;
+        resolve(project);
+      }
+    );
+  });
+}
+
+
+// --------------------------------------------------
+function getInvestigator(id) {
+  return new Promise(function (resolve, reject) {
+    connection.query(
+      'select * from investigator where investigator_id=?', 
+      [id], 
+      function (error, results, fields) {
+        if (error) reject(error);
+        if (results.length == 1) 
+          resolve(results[0]);
+        else 
+          reject("Bad investigator id: " + id);
+      }
+    );
+  });
+}
+
+// --------------------------------------------------
+function getProjectsForInvestigator(investigator) {
+  return new Promise(function (resolve, reject) {
+    connection.query(
+      `
+        select p.project_id, p.project_name 
+        from   project_to_investigator p2i, project p
+        where  p2i.investigator_id=?
+        and    p2i.project_id=p.project_id
+      `,
+      [investigator.investigator_id], 
+      function (error, results, fields) {
+        if (error) reject(error);
+        investigator['projects'] = results;
+        resolve(investigator);
+      }
+    );
+  });
+}
+
+
+// --------------------------------------------------
+function getSamplesForInvestigator(investigator) {
+  return new Promise(function (resolve, reject) {
+    connection.query(
+      `
+        select s.sample_id, s.sample_name, s.sample_type, 
+               s.latitude, s.longitude
+        from   project_to_investigator p2i, project p, sample s
+        where  p2i.investigator_id=?
+        and    p2i.project_id=p.project_id
+        and    p.project_id=s.project_id
+      `,
+      [investigator.investigator_id], 
+      function (error, results, fields) {
+        if (error) reject(error);
+        investigator['samples'] = results;
+        resolve(investigator);
+      }
+    );
+  });
+}
+
+// --------------------------------------------------
+function getFilesForSample(sample) {
+  return new Promise(function (resolve, reject) {
+    connection.query(
+      `
+        select f.sample_file_id, f.file, f.num_seqs, f.num_bp, f.avg_len,
+               t.sample_file_type_id, t.type as file_type
+        from   sample_file f, sample_file_type t
+        where  f.sample_id=?
+        and    f.sample_file_type_id=t.sample_file_type_id
+      `,
+      [sample.sample_id], 
+      function (error, results, fields) {
+        if (error) reject(error);
+        sample['files'] = results;
+        resolve(sample);
+      }
+    );
+  });
+}
+
+
+// --------------------------------------------------
+function getOntologiesForSample(sample) {
+  return new Promise(function (resolve, reject) {
+    connection.query(
+      `
+        select o.ontology_acc, o.label, 
+               t.type as ontology_type
+        from   sample_to_ontology s2o, ontology o, ontology_type t
+        where  s2o.sample_id=?
+        and    s2o.ontology_id=o.ontology_id
+        and    o.ontology_type_id=t.ontology_type_id
+      `,
+      [sample.sample_id], 
+      function (error, results, fields) {
+        if (error) reject(error);
+        sample['ontologies'] = results;
+        resolve(sample);
+      }
+    );
+  });
+}
+
+
+// --------------------------------------------------
+function connectMongo() {
+  return new Promise(function (resolve, reject) {
+    MongoClient.connect(MONGO_URL, (err, db) => {
+      if (err)
+        reject(err)
+      else 
+        resolve(db)
+    });
+  });
+}
+
+
+// --------------------------------------------------
+function getSampleKeys(db) {
+  /*
+   * Keys look like this:
+   *
+   * { _id: { key: 'specimen__volume' },
+   *  value: { types: { Number: 190, String: 16 } },
+   *  totalOccurrences: 206,
+   *  percentContaining: 4.092173222089789 }
+   *
+   * We need to take the most frequent of the "types"
+   * and return just
+   *
+   * { 'specimen__volume': 'number' }
+   *
+   */
+  return new Promise(function (resolve, reject) {
+    var col = db.collection('sampleKeys');
+    col.find().toArray(function(err, docs) {
+      if (err)
+        reject(err);    
+      else {
+        var keys = docs.filter((item) => {
+          var name = item._id.key;
+          return (name !== "" && name !== "_id");
+        }).reduce((acc, item) => {
+          var name  = item._id.key;
+          var types = item.value.types;
+          var type  = Object.keys(types)
+            .sort((a,b) => types[a] - types[b])
+            .reverse()[0];
+          acc[name] = type.toLowerCase();
+          return acc;
+        }, {});
+        resolve(keys);
+      }
+    });
+  });
+}
+
+
+// --------------------------------------------------
+function getMetaSearchResults(db, query) {
+  return new Promise(function (resolve, reject) {
+    if (typeof(query) == "object" && Object.keys(query).length > 0) {
+
+      qry = query
+      db.collection('sample').find(query).toArray(
+        function(err, docs) {
+          if (err) reject(err)
+          resolve(docs)
+        }
+      );
+    }
+    else {
+      reject("Bad query (" + JSON.stringify(query) + ")")
+    }
   });
 }
