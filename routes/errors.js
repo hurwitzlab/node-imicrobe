ae8e8dc2775133ec03eab3f1b788b8d666c52017
Matt Bomhoff <mbomhoff@email.arizona.edu>
2018-05-29 17:00:53 -0700

    Fixed errors

--- routes/errors.js
+++ routes/errors.js
@@ -1,5 +1,4 @@
 
-
 // Create error types
 class MyError extends Error {
     constructor(message, statusCode) {
@@ -12,3 +11,9 @@ const ERR_BAD_REQUEST = new MyError("Bad request", 400);
 const ERR_UNAUTHORIZED = new MyError("Unauthorized", 401);
 const ERR_PERMISSION_DENIED = new MyError("Permission denied", 403);
 const ERR_NOT_FOUND = new MyError("Not found", 404);
+
+module.exports.MyError = MyError;
+module.exports.ERR_BAD_REQUEST = ERR_BAD_REQUEST;
+module.exports.ERR_UNAUTHORIZED = ERR_UNAUTHORIZED;
+module.exports.ERR_PERMISSION_DENIED = ERR_PERMISSION_DENIED;
+module.exports.ERR_NOT_FOUND = ERR_NOT_FOUND;
