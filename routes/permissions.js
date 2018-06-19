163ff4d9f0f5c51b26afb20630d4d1f917c7f8ec
Matt Bomhoff <mbomhoff@email.arizona.edu>
2018-06-19 15:07:06 -0700

    Prevent permissions changes on scope data

--- routes/permissions.js
+++ routes/permissions.js
@@ -263,7 +263,9 @@ function requireProjectGroupPermission(projectGroupId, user, requiredPermission)
 function agaveUpdateFilePermissions(username, token, permission, files) {
     return Promise.all(
         files
-        .filter( f => !f.startsWith('/shared') ) // don't change permissions on files in the /iplant/home/shared dir
+        .map( f => f.replace(/^\/iplant\/home/) ) // remove leading "/iplant/home"
+        .filter( f => !f.startsWith('/shared') )  // don't change permissions on files in the /iplant/home/shared dir
+        .filter( f => !f.startsWith('/scope') )   // don't change permissions on files in the /iplant/home/scope dir
         .map(f => {
             return agaveGetFilePermissions(username, token, f)
                 .then( curPermission => {
